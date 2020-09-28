b6af2750a974327cc6ba108201cdd5ace777ad4d
Marco Marinò <51913759+marcomarinodev@users.noreply.github.com>
2020-09-28 22:25:43 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,17 +1,30 @@
 import random
 import matplotlib.pyplot as plt
-from Statistica import Statistica as s
+from DataStatistics import File as fsys
+from DataStatistics import DataStatistics as s
 from Utility import Utility as util
+import numpy as np
+import pandas as pd  # To read data
+from sklearn.linear_model import LinearRegression
 
 if __name__ == "__main__":
-    size = 20
-    x_dataset = [random.randrange(1, 101) for _ in range(size)]
-    y_dataset = [random.randrange(1, 101) for _ in range(size)]
 
-    x_stat = s.Statistica(x_dataset)
-    y_stat = s.Statistica(y_dataset)
+    (x_dataset, y_dataset) = fsys.File('data.csv').get_csv_data()
 
-    print(x_stat.get_dataset())
+    x_stat = s.DataStatistics(x_dataset)
+    y_stat = s.DataStatistics(y_dataset)
+
+    # Regression line using sklearn
+    data = pd.read_csv('data.csv')  # load data set
+    X = data.iloc[:, 1].values.reshape(-1, 1)  # values converts it into a numpy array
+    Y = data.iloc[:, 2].values.reshape(-1, 1)  # -1 means that calculate the dimension of rows, but have 1 column
+    linear_regressor = LinearRegression()  # create object for the class
+    linear_regressor.fit(X, Y)  # perform linear regression
+    Y_pred = linear_regressor.predict(X)  # make predictions
+
+    plt.scatter(X, Y)
+    plt.plot(X, Y_pred, color='red')
+    plt.show()
 
     # plot x data
     x_stat.plot_data("X array")
@@ -23,12 +36,13 @@ if __name__ == "__main__":
     plt.plot(x, y * 100, linestyle='dotted', lw=2)
     plt.show()
 
+    ### ISSUE TO SOLVE
     # Covariance and regression line
-    x_cov = x_stat.covariance(y_stat)
-    b = x_cov / y_stat.variance()
-    a = y_stat.mean() - b * x_stat.mean()
+    # x_cov = x_stat.covariance(y_stat)
+    # b = x_cov / y_stat.variance()
+    # a = y_stat.mean() - b * x_stat.mean()
 
-    plt.scatter(x_dataset, y_dataset)
-    plt.title("Regression line and x, y correlation")
-    util.create_line([0, a], [1, b + a])
-    plt.show()
+    # plt.scatter(x_dataset, y_dataset)
+    # plt.title("Regression line and x, y correlation")
+    # util.create_line([0, a], [1, b + a])
+    # plt.show()
